43799258841b6abd3a8f39844a432d5e0351abf1
Vladimir Ermakov <vooon341@gmail.com>
2014-12-05 17:31:26 +0300

    gpsd: Add program options, logging.

--- xat_gpsd/src/xat_gpsd.cpp
+++ xat_gpsd/src/xat_gpsd.cpp
@@ -1,12 +1,50 @@
 /* dummy node
  */
 
-#include <boost/shared_ptr.hpp>
+#include <boost/program_options.hpp>
+#include <console_bridge/console.h>
 #include <lcm/lcm-cpp.hpp>
+#include <libgpsmm.h>
 
 #include "xat_msgs/gps_fix_t.hpp"
 
+namespace po = boost::program_options;
+
+
 int main(int argc, char *argv[])
 {
+	bool debug;
+	std::string gps_host;
+	std::string gps_port;
+	std::string lcm_url;
+
+	po::options_description desc("options");
+	desc.add_options()
+		("help", "produce help message")
+		("lcm-url,l", po::value(&lcm_url), "LCM connection URL")
+		("gps-host,h", po::value(&gps_host)->default_value("127.0.0.1"), "Host running GPSD, IP or DNS address")
+		("gps-port,p", po::value(&gps_port)->default_value("2947"), "GPSD port")
+		("debug,d", po::bool_switch(&debug)->default_value(false), "Emit debug information")
+		;
+	po::variables_map vm;
+	po::store(po::parse_command_line(argc, argv, desc), vm);
+	po::notify(vm);
+
+	if (vm.count("help"))
+	{
+		std::cout << desc << std::endl;
+		return 1;
+	}
+
+	if (debug)
+		console_bridge::setLogLevel(console_bridge::CONSOLE_BRIDGE_LOG_DEBUG);
+	else
+		console_bridge::setLogLevel(console_bridge::CONSOLE_BRIDGE_LOG_INFO);
+
+	logInform("XAT-GPSd: initializing");
+	logDebug("XAT-GPSd: debug enabled");
+
+	/* TODO */
+
 	return EXIT_SUCCESS;
 }
