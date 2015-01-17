eab9cff3f4bd55dfc8ef8b65908adfdd8e9101d0
Vladimir Ermakov <vooon341@gmail.com>
2015-01-17 12:02:58 +0300

    scripts: initial import

---
+++ xat_scripts/scripts/xat_stop.lua
@@ -0,0 +1,19 @@
+#!/usr/bin/env lua
+
+local lcm = require('lcm')
+
+-- this might be necessary depending on platform and LUA_PATH
+package.path = './?/init.lua;' .. package.path
+
+local xat_msgs = require('xat_msgs')
+
+local lc = lcm.lcm.new()
+
+local msg = xat_msgs.command_t:new()
+
+-- XXX: find how to produce right header (seq+stamp)
+msg.header = xat_msgs.header_t:new()
+msg.command = 200
+
+lc:publish("xat/command", msg:encode())
+print "Stop command published."
